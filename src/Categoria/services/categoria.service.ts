093677b7dc7c1f610aa903c605f6207fafceccf9
Leticia Fontes <leticia.abib95@gmail.com>
2025-07-08 14:52:38 -0300

    create: criação de produto e CRUD

--- src/Categoria/services/categoria.service.ts
+++ src/Categoria/services/categoria.service.ts
@@ -12,13 +12,20 @@ export class CategoriaService{
         private categoriaRepository: Repository<Categoria>
     ) {}
     async findAll(): Promise<Categoria[]>{
-        return await this.categoriaRepository.find();
+        return await this.categoriaRepository.find({
+             relations:{
+                produto: true
+            }
+        });
     }
 
     async findById(id:number): Promise<Categoria>{
         const categoria = await this.categoriaRepository.findOne({
             where:{
                 id
+            }, 
+            relations:{
+                produto: true
             }
         });
 
@@ -32,6 +39,9 @@ export class CategoriaService{
         return await this.categoriaRepository.find({
             where:{
                 nome: ILike(`%$[nome]%`)
+            }, 
+            relations:{
+                produto: true
             }
         })
     }
